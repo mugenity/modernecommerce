fde6d6914430aa33b2255f9ce5e863bb2a4fa82b
MuGenFJ <63530527+MuGenFJ@users.noreply.github.com>
2021-02-13 23:25:04 +0100

    Homepage and routing and styles

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.scss";
+
+function Footer(props) {
+  return (
+    <footer className="footer">
+      <div className="wrap">Copyright 2020</div>
+    </footer>
+  );
+}
+
+export default Footer;
