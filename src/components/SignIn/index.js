d640263cbd017d44a1417c6f84b048529a58f107
MuGenFJ <63530527+MuGenFJ@users.noreply.github.com>
2021-02-14 21:59:21 +0100

    SignIn & SignUp Firebase

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,14 +1,47 @@
 import React, { Component } from "react";
 import Button from "../Forms/Button";
-import { signInWithGoogle } from "./../../firebase/utils";
+import { signInWithGoogle, auth } from "./../../firebase/utils";
+import FormInput from "./../Forms/FormInput";
 import "./styles.scss";
 
+const initialState = {
+  email: "",
+  password: "",
+};
+
 class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ...initialState,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   handleSubmit = async (e) => {
     e.preventDefault();
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({
+        ...initialState,
+      });
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   render() {
+    const { email, password } = this.state;
     return (
       <div className="signin">
         <div className="wrap">
@@ -16,6 +49,23 @@ class SignIn extends Component {
 
           <div className="formWrap">
             <form onSubmit={this.handleSubmit}>
+              <FormInput
+                type="email"
+                name="email"
+                value={email}
+                placeholder="Email"
+                handleChange={this.handleChange}
+              />
+              <FormInput
+                type="password"
+                name="password"
+                value={password}
+                placeholder="Password"
+                handleChange={this.handleChange}
+              />
+
+              <Button type="submit">LogIn</Button>
+
               <div className="socialSignin">
                 <div className="row">
                   <Button onClick={signInWithGoogle}>
